ad9783440bb894ef5fec68a9dcfdca7296adcf32
Kasper Laursen <habberlabber@gmail.com>
2021-08-18 19:52:40 +0200

    Add post list to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch(`/index.json`);
+    const { posts } = await res.json();
+    return {
+      props: { posts },
+    };
+  }
+</script>
+
+<script>
+  export let posts;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each posts as post}
+  <li>
+    <a href="/{post.slug}">
+      {post.title}
+    </a>
+  </li>
+{/each}
