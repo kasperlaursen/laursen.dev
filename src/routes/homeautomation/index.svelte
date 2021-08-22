b5963c521a0d5ef798d0afdc582d1e80b5f60240
kasperlaursen <habberlabber@gmail.com>
2021-08-22 17:45:47 +0200

    WIP

--- src/routes/homeautomation/index.svelte
+++ src/routes/homeautomation/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Diagram from '$lib//homeautomation/Diagram.svelte';
 </script>
 
 <h1>Home Automation</h1>
@@ -6,6 +7,7 @@
 	Thanks for visiting. <br />
 	Unfortunately this page is still a work in progress, check back later!
 </p>
+<Diagram />
 
 <style>
 </style>
